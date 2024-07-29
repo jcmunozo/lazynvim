62d67424c3b0c78352f9ef41c6e4770acf88e8d1
jcmunozo <jcmunozo@unicauca.edu.co>
2024-07-28 19:17:02 -0500

    Using lazy.nvim recomendations

--- lua/plugins/alternate.lua
+++ lua/plugins/alternate.lua
@@ -1,5 +1,18 @@
 return {
   'rmagatti/alternate-toggler',
   event = "VeryLazy",
-  config = true,
+  opts = {
+    alternates = {
+      ["=="] = "!=",
+    }
+  },
+  config = function(_, opts)
+    require("alternate-toggler").setup(opts)
+    vim.keymap.set(
+      "n",
+      "<leader><space>",   -- <space><space>
+      "<cmd>lua require('alternate-toggler').toggleAlternate()<CR>",
+      { noremap = true, silent = true }
+    )
+  end
 }
