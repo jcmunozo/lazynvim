1542db2c99fe13d81e8c76069545a3731385628d
jcmunozo <jcmunozo@unicauca.edu.co>
2024-10-14 20:35:14 -0500

    Updated tokionight plugin

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -6,13 +6,10 @@ return {
     require("tokyonight").setup({
       -- Choose the style: "storm", "moon", "night", or "day"
       style = "moon",
-      
       -- Enable transparency
       transparent = true,
-      
       -- Terminal colors
       terminal_colors = true,
-      
       -- Styles for syntax elements
       styles = {
         comments = { italic = true },
@@ -20,18 +17,14 @@ return {
         functions = {},
         variables = {},
       },
-      
       -- Disable italic for specific items
       on_highlights = function(hl, c)
         hl.DiagnosticUnnecessary = { fg = c.comment, italic = false }
       end,
-      
       -- Sidebars
       sidebars = { "qf", "help" },
-      
       -- Day style brightness (0-1)
       day_brightness = 0.3,
-      
       -- Other options
       hide_inactive_statusline = false,
       dim_inactive = false,
